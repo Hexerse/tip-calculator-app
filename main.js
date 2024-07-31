06dfd387b42c974d95c259d8f67270406a554434
Hexerse <lucaschuat@hotmail.com>
2024-07-31 09:33:10 +0800

    Finishing tipping and total system

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-const form = 
+const form = document.getElementById("form");
+const buttons = document.querySelectorAll(".tip-button");
+const custom = document.getElementById("custom");
+const bill = document.getElementById("bill");
+const people = document.getElementById("people");
+
+const calcTip = document.querySelector(".calc-tip");
+const totalPerson = document.querySelector(".calc-total");
+
+let billValue = 0;
+let peopleValue = 0;
+let tipValue = 0;
+
+let billperPerson = 0;
+let tipperPerson = 0;
+
+const handleBill = (bill) => {
+  billValue = parseFloat(bill.target.value);
+};
+
+const handlePeople = (people) => {
+  peopleValue = parseInt(people.target.value);
+};
+
+const handleTip = (e) => {
+  tipValue = parseFloat(e.target.value.replace("%", "") / 100);
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleTip);
+});
+
+const tipAmount = (bill, tip, people) => {
+  if (bill && tip && people) {
+    billperPerson = bill / people;
+    tipperPerson = (bill * tip) / people;
+    changeAmt(billperPerson, tipperPerson);
+  }
+};
+
+const changeAmt = (billperPerson, tipperPerson) => {
+  calcTip.innerHTML = `$ ${tipperPerson} `;
+  totalPerson.innerHTML = `$ ${billperPerson} `;
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  tipAmount(billValue, tipValue, peopleValue);
+};
+
+custom.addEventListener("keyup", handleTip);
+const submit = form.addEventListener("submit", handleSubmit);
+const getBill = bill.addEventListener("focusout", handleBill);
+const peopleNum = people.addEventListener("keyup", handlePeople);
